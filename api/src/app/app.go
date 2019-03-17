c10ff4671c58cd1221ef580521957b622dfed0f8
juankml <juancarlos.ojedagomez@mercadolibre.com>
2019-03-17 20:26:16 -0300

    start new proyect registry

---
+++ api/src/app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+//Start function to start up proyect
+func Start() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
