694bea2219063afc574ee37de173e0877c993baf
juan carlos ojeda <juancarlos.ojedagomez@mercadolibre.com>
2019-04-28 11:29:49 -0300

    deploy ansible, terraform

--- deploy/infrastructure/security_group.tf
+++ deploy/infrastructure/security_group.tf
@@ -2,7 +2,6 @@ resource "aws_security_group" "allow_ssh_anywhere" {
   name        = "allow_ssh_anywhere"
   description = "Allow all inbound traffic to ssh"
   vpc_id      = "vpc-7763ea0d" #Aca tomar de AWS directamente
-
   ingress {
     from_port   = 22
     to_port     = 22
@@ -10,12 +9,12 @@ resource "aws_security_group" "allow_ssh_anywhere" {
     cidr_blocks = [ "0.0.0.0/0" ]
     }
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "0.0.0.0/0"]
     description = "Allow access from any host"
-    }  
+    }
   egress {
     from_port       = 0
     to_port         = 0
